5b08fd514e8ebf251a8d50896fd79797186508d0
OFF <peerapat13th@gmail.com>
2024-10-07 03:08:35 +0700

    Models is done

---
+++ Models/RoleModel.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Models;
+
+public class RoleModel : IdentityRole<Guid>
+{
+    public string? Description { get; set; }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Models;
+
+public class UserModel : IdentityUser<Guid>
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
+
+    /// <summary>
+    /// Products that own by this user
+    /// </summary>
+    public ICollection<ProductModel> Products { get; set; } = new List<ProductModel>();
+}
+
