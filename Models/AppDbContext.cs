90208a58d2745502febe54ca258b2df9b62a8235
OFF <peerapat13th@gmail.com>
2024-10-16 23:01:08 +0700

    Add UseModel and RoleModel

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,17 +1,33 @@
 using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebAPI.Models;
 
-public class AppDbContext : DbContext
+//New class and Inherit from IdentityDbContext
+public class AppDbContext : IdentityDbContext<UserModel, RoleModel, Guid>
 {
-
+    //New Table in DB
     public DbSet<ProductModel> Products { get; set; }
 
+    //Constructor
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
-    
+    //OnModelCreating Method to modify DB Table when migration
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Call base OnModelCreating Method
+        base.OnModelCreating(builder);
+
+        //Seed data(Add data when migration) for RoleModel
+        builder.Entity<RoleModel>().HasData(
+            new RoleModel { Id = Guid.Parse("dfb04ae0-0184-41ce-ac5d-1bee1ade19b3"), Name = "Customer", NormalizedName = "CUSTOMER", Description = "Customer role" },
+            new RoleModel { Id = Guid.Parse("d1b172ba-4d15-4505-8de0-b43588da3359"), Name = "Seller", NormalizedName = "SELLER", Description = "Seller role" }
+        );
+    }
+
+
 }
 
--- obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9cd06bb6ec8d7c8e1e4df177b5bbd729acaea4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3089c2dcc64af76edb5ac07138469b3d1626d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
